37ed58df0053b01ad01eeeadcbb286c4f3ecf397
J. nathan Matias <natematias@gmail.com>
2011-09-26 22:49:11 -0400

    updated code to check array size for arrays

--- utilities/aggregate_data_from_files.rb
+++ utilities/aggregate_data_from_files.rb
@@ -23,16 +23,16 @@ Dir.glob(File.join(dirname, "**", "*.xml")).each do |filename|
   if filename != "." and filename != ".."
     article = Article.new(filename)
     total_articles += 1 if article
-    article_data[:bylines]     += 1 if article.bylines
-    article_data[:locations]   += 1 if article.locations
-    article_data[:descriptors] += 1 if article.descriptors
+    article_data[:bylines]     += 1 if article.bylines.size > 0
+    article_data[:locations]   += 1 if article.locations.size > 0
+    article_data[:descriptors] += 1 if article.descriptors.size > 0
     article_data[:news_desks]  += 1 if article.news_desk
     article_data[:datelines]   += 1 if article.dateline
     article_data[:pages]       += 1 if article.page
     article_data[:sections]    += 1 if article.section
     article_data[:columns]     += 1 if article.column
     article_data[:publication_dates] += 1 if article.publication_date
-    article_data[:taxonomic_classifiers] += 1 if article.taxonomic_classifiers
+    article_data[:taxonomic_classifiers] += 1 if article.taxonomic_classifiers.size > 0
  
     counter += 1
     print "." if counter % 20 == 0
