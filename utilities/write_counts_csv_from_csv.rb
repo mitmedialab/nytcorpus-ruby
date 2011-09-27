fe703d29518d5b9d39e8a44ad699df7b9a699699
Rahul Bhargava <rahulb@media.mit.edu>
2011-09-27 13:36:32 -0400

    added doc on how to create summary CSV, added front-page filter to counter script

--- utilities/write_counts_csv_from_csv.rb
+++ utilities/write_counts_csv_from_csv.rb
@@ -4,9 +4,11 @@ require 'ftools'
 require 'pp'
 require 'csv'
 
+Front_Page_Only = true
+Col_Index_In_Csv = 4  # this is the col to filter for
+
 csvname = ARGV[0]
 output_csv = ARGV[1]
-
 counts = Hash.new
 
 row_index = 0
@@ -25,13 +27,29 @@ $stdout.flush
 
 all_rows.each do |row|
   if row_index > 0
-    row[9].split(",").each do |value|
-      if not counts.has_key?(value)
-        counts[value] = {:stories=>0, :words=>0}
+
+    is_front_page = (row[7]=="A") && (row[6].to_i==1)
+    
+    if (not Front_Page_Only) || (is_front_page && Front_Page_Only)
+      
+      word_count = 0
+      if numeric?(row[10].to_s)
+        word_count = row[10].to_i
       end
-      counts[value][:stories] += 1
-      counts[value][:words] += row[10].to_i if numeric?(row[10].to_s)
+      
+      raw_value = row[Col_Index_In_Csv]
+      if not raw_value.nil?
+        raw_value.split(",").each do |value|
+          if not counts.has_key?(value)
+            counts[value] = {:stories=>0, :words=>0}
+          end
+          counts[value][:stories] += 1
+          counts[value][:words] += word_count
+        end
+      end
+      
     end
+    
   end
   row_index += 1
 end
